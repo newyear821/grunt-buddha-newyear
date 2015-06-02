a885d4ff0a8a4b4a15ff159f626ebeeec07f0839
newyear <525990956@qq.com>
2015-06-02 16:29:23 +0800

    finish

---
+++ Gruntfile.js
@@ -0,0 +1,60 @@
+/*
+ * grunt-buddha-newyear
+ * 
+ *
+ * Copyright (c) 2015 newyear
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+module.exports = function (grunt) {
+  // load all npm grunt tasks
+  require('load-grunt-tasks')(grunt);
+
+  // Project configuration.
+  grunt.initConfig({
+    jshint: {
+      all: [
+        'Gruntfile.js',
+        'tasks/*.js',
+        '<%= nodeunit.tests %>'
+      ],
+      options: {
+        jshintrc: '.jshintrc',
+        reporter: require('jshint-stylish')
+      }
+    },
+
+    // Before generating any new files, remove any previously-created files.
+    clean: {
+      tests: ['tmp']
+    },
+
+    // Configuration to be run (and then tested).
+    buddha_newyear: {
+      options: {
+        who: 'buddha', //buddha alpaca
+        commentSymbol: '//'
+      },
+      dist: ['test/fixtures/*.js']
+    },
+
+    // Unit tests.
+    nodeunit: {
+      tests: ['test/*_test.js']
+    }
+
+  });
+
+  // Actually load this plugin's task(s).
+  grunt.loadTasks('tasks');
+
+  // Whenever the "test" task is run, first clean the "tmp" dir, then run this
+  // plugin's task(s), then test the result.
+  grunt.registerTask('test', ['clean', 'buddha_newyear', 'nodeunit']);
+
+  // By default, lint and run all tests.
+  grunt.registerTask('default', ['jshint', 'test']);
+
+};
